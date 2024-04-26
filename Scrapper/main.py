a544ac3ca13918cb227a0d54da48f606358cf8aa
Adlen CHERIF <adlenc@apitech.fr>
2024-04-26 17:07:19 +0200

    Update source URL in conf.json and remove print statements in main.py

--- Scrapper/main.py
+++ Scrapper/main.py
@@ -4,6 +4,7 @@ from bs4 import BeautifulSoup as bs
 from model.config import Config
 from model.meal import Meal, MealType, FoodItem
 from utils import parse_date
+import sqlite3 as sql
 
 firefox_headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:89.0) Gecko/20100101 Firefox/89.0',
@@ -24,7 +25,7 @@ def getMenu(url, headers):
     returnMeals: list[Meal] = []
 
     meals = soup.find_all("div", class_="meal")
-    print(meals)
+    # print(meals)
     for meal in meals:
         newMeal = Meal(parse_date(date.replace("Menu du ", "").strip()), "", [], MealType.BREAKFAST)
         mealType = meal.find("div", class_="meal_title").text
@@ -37,20 +38,30 @@ def getMenu(url, headers):
         else:
             continue
 
-        foods = meal.find("ul", class_="meal_foodies").find_all("li")
-        print(foods)
+        foods = meal.find("ul", class_="meal_foodies").find_all("li", recursive=False)
+        # print(foods)
 
         for food in foods:
-            title = food.text
+            title = food.contents[0].text
             newMeal.title = title
+            newMeal.foodItems = []
             items = food.find_all("li")
             for item in items:
                 name = item.text.strip()
                 newMeal.foodItems.append(FoodItem(name, 0))
+            returnMeals.append(newMeal.toJsonObject())
+
+            print(f"found {len(newMeal.foodItems)} items for {mealType.name} : {newMeal.title}")
+            print(f"List of items:" + '\n'.join([str(foodItem) for foodItem in newMeal.foodItems]) + '\n\n')
         
-        returnMeals.append(newMeal)
 
     return returnMeals
+
+def dbConnection():
+    conn = sql.connect('Database\data.db')
+    cursor = conn.cursor()
+    cursor.execute('CREATE TABLE IF NOT EXISTS meals (date TEXT, title TEXT, foodItems TEXT, mealType TEXT)')
+    return conn, cursor
 
 if __name__ == '__main__':
     config = Config.loadConfig()
@@ -63,4 +74,4 @@ if __name__ == '__main__':
     # items = soup.find("section", class_="vc_restaurants").find_all("li")
     # print(items)
 
-    print(getMenu("https://www.crous-lyon.fr/restaurant/restaurant-manufacture-des-tabacs/", firefox_headers))
+    getMenu("https://www.crous-lyon.fr/restaurant/cafeteria-saint-paul/", firefox_headers)
--- Scrapper/model/meal.py
+++ Scrapper/model/meal.py
@@ -1,3 +1,4 @@
+from collections.abc import Iterable
 from enum import Enum
 
 class MealType(Enum):
@@ -13,14 +14,30 @@ class FoodItem:
     def __str__(self):
         return f'{self.name} - {self.price}'
     
+    def __dict__(self):
+        return {
+            'name': self.name,
+            'price': self.price
+        }
+    
 class Meal:
     def __init__(self, date, title, foodItems: list[FoodItem], mealType: MealType):
         self.title = title
         self.foodItems = foodItems
         self.mealType = mealType
+        self.date = date
 
     def __str__(self):
         return f'{self.title}' + '\n' + '\n'.join([f'{foodItem.name} - {foodItem.price}' for foodItem in self.foodItems])
     
     def __repr__(self):
         return f'{self.title}' + '\n' + '\n'.join([f'{foodItem.name} - {foodItem.price}' for foodItem in self.foodItems])
+    
+    def toJsonObject(self):
+        return {
+            'date': self.date,
+            'title': self.title,
+            'foodItems': [foodItem.__dict__ for foodItem in self.foodItems],
+            'mealType': self.mealType.name
+        }
+
